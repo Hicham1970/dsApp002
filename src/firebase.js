49336866288857637efe0e4613afc43c59fe4421
Hicham1970 <devhicham2@gmail.com>
2025-03-27 01:04:12 +0000

    add .env and tweak firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAZRX4Q1gT70DdeCferGuZlTNzq6FwT6EU",
-  authDomain: "firstfirebaseproject-f2451.firebaseapp.com",
-  projectId: "firstfirebaseproject-f2451",
-  storageBucket: "firstfirebaseproject-f2451.firebasestorage.app",
-  messagingSenderId: "99207712858",
-  appId: "1:99207712858:web:789d97b25d15c07da44863",
-  measurementId: "G-FCT1EP4VQL"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -18,7 +20,5 @@ const app = initializeApp(firebaseConfig);
 // Make auth and firestore references
 const authInstance = getAuth(app);
 const db = getFirestore(app);
-
-
 
 export { db, authInstance as auth }
